4dd0b697dcebd0008212e9233596bff8e6813813
rianjskis <86937445+rianjskis@users.noreply.github.com>
2024-01-29 22:41:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,6 @@
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/luci/themes/luci-theme-argon
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
@@ -22,4 +21,3 @@ git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone https://github.com/zxl78585/luci-app-filetransfer.git package/luci-app-filetransfer
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/luci-app-ddns-go
