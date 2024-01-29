2621b51ad04876dce3528385f88300020e57e01c
rianjskis <86937445+rianjskis@users.noreply.github.com>
2024-01-29 15:44:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
-sed -i 's#^root:::0:99999:7:::#root:$1$O73yltLo$SBOqN9kzSHZA5EuKYWXmG/::0:99999:7:::#' package/base-files/files/etc/shadow
