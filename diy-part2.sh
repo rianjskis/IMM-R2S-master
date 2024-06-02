e8b2cf7a336259b922bfcd02466308e943796dbd
rianjskis <86937445+rianjskis@users.noreply.github.com>
2024-06-02 12:09:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP跟时区
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 sed -i "s/set system.@system\[-1\].timezone='UTC'/set system.@system[-1].timezone='CST-8'/g; /set system.@system\[-1\].timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
 # 插件
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-openclash
