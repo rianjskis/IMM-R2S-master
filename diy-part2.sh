4efa5c85767982dd124cd208f3c4d4a20193e4d9
rianjskis <86937445+rianjskis@users.noreply.github.com>
2024-11-07 09:44:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP跟时区
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 # 插件
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/luci/applications/luci-app-openclash
 #rm -rf feeds/luci/applications/luci-app-homeproxy
 #git clone -b dev https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
