71475867f1068787a6a044a2725bfbb1b4c7ee15
rianjskis <86937445+rianjskis@users.noreply.github.com>
2026-01-23 10:59:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,11 +17,11 @@ s/set system.@system\[-1\].timezone='UTC'/set system.@system\[-1\].timezone='CST
 a\                set system.@system[-1].zonename='Asia/Shanghai'
 }" package/base-files/files/bin/config_generate
 # 插件
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-mwan3
-rm -rf feeds/luci/applications/luci-app-syncdial
-rm -rf feeds/packages/mwan3
+rm -rf package/feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/luci/applications/luci-app-openclash
+rm -rf package/feeds/luci/applications/luci-app-mwan3
+rm -rf package/feeds/luci/applications/luci-app-syncdial
+rm -rf package/feeds/packages/mwan3
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 sed -i '$a /root' package/base-files/files/etc/sysupgrade.conf    # 编译时默认插入root目录,升级时root目录不清空
 sed -i '$a /etc/init.d/pwm-fan' package/base-files/files/etc/sysupgrade.conf
